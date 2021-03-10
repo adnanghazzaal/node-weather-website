e3fecc214580bcdff27cd07717312ecf1b1b9957
Adnan Ghazzaal <adnanghazzaal@gmail.com>
2021-03-10 20:24:58 +0500

    Code Fixed For API Call to server

--- public/js/app.js
+++ public/js/app.js
@@ -8,32 +8,35 @@ form.addEventListener("submit", (e) => {
   const input = document.querySelector("input").value;
   location.textContent = "Getting Location";
   fetch(
-    `http://api.weatherstack.com/current?access_key=1247f1b0348455c20a35a28a91e553bb&query=${input}`
+    // `http://api.weatherstack.com/current?access_key=1247f1b0348455c20a35a28a91e553bb&query=${input}`
+    `http://localhost:3000/weather?address=${input}`
   )
     .then((res) => {
       res.json().then((data) => {
-        console.log(data.error);
+        location.textContent = "Getting Location";
         if (data.error) {
+          console.log(data.error);
           weather.textContent = `Location Not Found. Please Check`;
-          location.textContent = "Getting Location";
           return;
         } else {
-          console.log(data.current);
+          console.log(data.weather);
           console.log(data.location);
-          location.textContent = `Weather at ${data.location.name} is currently`;
+          location.textContent = data.location;
+          // `Weather at ${data.location.name} is currently`;
 
           //   for (const property in data.current) {
           //     const p = document.createElement("p");
           //     p.innerHTML = `${property} : ${data.current[property]}`;
           //     weather.appendChild(p);
           //   }
-          weather.innerHTML = `
-            <h3>${data.current.weather_descriptions[0]}</h3>
-            <p>Feels Like :${data.current.feelslike}</p>
-            <p>Humidity: ${data.current.humidity}</p>
-            <p>Visibility: ${data.current.visibility}</p>
+          // weather.innerHTML = `
+          //   <h3>${data.current.weather_descriptions[0]}</h3>
+          //   <p>Feels Like :${data.current.feelslike}</p>
+          //   <p>Humidity: ${data.current.humidity}</p>
+          //   <p>Visibility: ${data.current.visibility}</p>
 
-            `;
+          //   `;
+          weather.textContent = data.weather;
         }
       });
     })
