1a73a41c83c9324e4ba94397b04029697c29db17
Adnan Ghazzaal <adnanghazzaal@gmail.com>
2021-03-10 20:23:29 +0500

    Fixed weather api call to node server

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const foreCast = require("../src/Utils/weather.js");
 const geocode = require("../src/Utils/geocode.js");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define Paths for Express Config
 const viewsPath = path.join(__dirname, "/templates/views");
@@ -98,6 +99,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
